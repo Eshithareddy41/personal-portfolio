4623a93e363182861a75e10fe713f1c51fcdea29
Eshithareddy41 <eshureddy20@gmail.com>
2026-08-02 00:23:27 +0530

    Update portfolio website

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 const themeToggle = document.getElementById("themeToggle");
 const body = document.body;
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+  body.classList.add("light");
+  themeToggle.textContent = "☀️";
+} else {
+  themeToggle.textContent = "🌙";
+}
 
 themeToggle.addEventListener("click", () => {
   body.classList.toggle("light");
 
   if (body.classList.contains("light")) {
-    themeToggle.textContent = "🌙";
-  } else {
     themeToggle.textContent = "☀️";
+    localStorage.setItem("theme", "light");
+  } else {
+    themeToggle.textContent = "🌙";
+    localStorage.setItem("theme", "dark");
   }
+});
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 150;
+
+    if (window.scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
 });
